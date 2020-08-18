7a83c28407e2566d98278acd9ecfb0d2a85a290c
wangbo <1216476406@qq.com>
2020-08-18 10:18:36 +0800

    LeetCode 1480.一维数组的动态和

---
+++ algorithm/LeetCode/No1480.java
@@ -0,0 +1,36 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class No1480 {
+
+//    给你一个数组 nums 。数组「动态和」的计算公式为：runningSum[i] = sum(nums[0]…nums[i]) 。
+//
+//    请返回 nums 的动态和。
+//
+//    输入：nums = [1,2,3,4]
+//    输出：[1,3,6,10]
+//    解释：动态和计算过程为 [1, 1+2, 1+2+3, 1+2+3+4] 。
+//    示例 2：
+//
+//    输入：nums = [1,1,1,1,1]
+//    输出：[1,2,3,4,5]
+//    解释：动态和计算过程为 [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1] 。
+//    示例 3：
+//
+//    输入：nums = [3,1,2,10,1]
+//    输出：[3,4,6,16,17]
+
+    public static void main(String[] args) {
+        int[] nums = {3,1,2,10,1};
+        System.out.println(Arrays.toString(runningSum(nums)));
+    }
+
+    public static int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i-1] + nums[i];
+        }
+        return nums;
+    }
+
+}
