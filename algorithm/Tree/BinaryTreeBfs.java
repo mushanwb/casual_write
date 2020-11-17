4b0e25468cdae50d1318c11e19b44fb35097f24c
wangbo <1216476406@qq.com>
2020-11-17 14:36:22 +0800

    二叉树的层次遍历

--- algorithm/Tree/BinaryTreeBfs.java
+++ algorithm/Tree/BinaryTreeBfs.java
@@ -18,35 +18,86 @@ public class BinaryTreeBfs {
     }
 
     public static void main(String[] args) {
-        BinaryTreeBfs.TreeNode node1 = new TreeNode(1);
-        BinaryTreeBfs.TreeNode node2 = new TreeNode(2);
-        BinaryTreeBfs.TreeNode node3 = new TreeNode(3);
-        BinaryTreeBfs.TreeNode node4 = new TreeNode(4);
-        BinaryTreeBfs.TreeNode node5 = new TreeNode(5);
-        BinaryTreeBfs.TreeNode node6 = new TreeNode(6);
-        BinaryTreeBfs.TreeNode node7 = new TreeNode(7);
-        BinaryTreeBfs.TreeNode node8 = new TreeNode(8);
-        BinaryTreeBfs.TreeNode node9 = new TreeNode(9);
-        BinaryTreeBfs.TreeNode node10 = new TreeNode(10);
+        BinaryTreeBfs.TreeNode node1 = new TreeNode(3);
+        BinaryTreeBfs.TreeNode node2 = new TreeNode(9);
+        BinaryTreeBfs.TreeNode node3 = new TreeNode(20);
+        BinaryTreeBfs.TreeNode node4 = new TreeNode(15);
+        BinaryTreeBfs.TreeNode node5 = new TreeNode(7);
 
         node1.left = node2;
         node1.right = node3;
 
-        node2.left = node4;
-        node2.right = node5;
-
-        node3.right = node6;
-
-        node4.left = node8;
-        node4.right = node9;
-
-        node6.left = node7;
-        node6.right = node10;
+        node3.left = node4;
+        node3.right = node5;
 
         System.out.println(bfs(node1));
     }
 
-    public static List<Integer> bfs(TreeNode root) {
+
+    // 将二叉树遍历成下面这个结构
+//                3
+//               / \
+//               9  20
+//               /  \
+//               15   7
+
+//          [
+//            [3],
+//            [9,20],
+//            [15,7]
+//          ]
+
+    public static List<List<Integer>> bfs(TreeNode root) {
+
+        List<List<Integer>> val = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        Queue<Integer> num = new LinkedList<>();
+        q.add(root);
+
+        num.add(1);
+        int a = 0;
+        int b = 0;
+        List<Integer> tempVal = new ArrayList<>();
+
+        while (q.size() != 0) {
+            TreeNode temp = q.poll();
+            tempVal.add(temp.value);
+            a++;
+
+            if (temp.left != null) {
+                q.add(temp.left);
+                b++;
+            }
+
+            if (temp.right != null) {
+                q.add(temp.right);
+                b++;
+            }
+
+            if (a == num.peek()) {
+                val.add(tempVal);
+                a = 0;
+                num.poll();
+                tempVal = new ArrayList<>();
+                num.add(b);
+                b = 0;
+            }
+        }
+
+        return val;
+    }
+
+
+
+// 将二叉树遍历成下面这个结构
+//                3
+//               / \
+//               9  20
+//               /  \
+//               15   7
+
+//          [3,9,20,15,7]
+    public static List<Integer> bfs2(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         Queue<BinaryTreeBfs.TreeNode> queue = new LinkedList<>();
         queue.add(root);
